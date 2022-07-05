2ee4071641db3c607a2516c661d8a212e2b97d3d
ryutaromiyagawa <miyagaworld_0912@icloud.com>
2022-07-05 11:54:45 +0900

    2章：ユーザー登録機能を作ろう

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,7 +7,6 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    //==========ここから追加==========
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
